81f2265614a26cb3f063998aca529fd2092a3a63
Azamshah <89238534+Kir0Sploit@users.noreply.github.com>
2022-07-07 23:02:11 +0800

    Update ytcli.sh

--- ytcli.sh
+++ ytcli.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
+# Ansi color code variables
+red="\e[0;91m"
+blue="\e[0;94m"
+expand_bg="\e[K"
+blue_bg="\e[0;104m${expand_bg}"
+red_bg="\e[0;101m${expand_bg}"
+green_bg="\e[0;102m${expand_bg}"
+green="\e[0;92m"
+white="\e[0;97m"
+bold="\e[1m"
+uline="\e[4m"
+reset="\e[0m"
+echo -e "${green}
+
+░█──░█ ▀▀█▀▀ ░█▀▀█ ░█─── ▀█▀ 
+░█▄▄▄█ ─░█── ░█─── ░█─── ░█─ 
+──░█── ─░█── ░█▄▄█ ░█▄▄█ ▄█▄
+${reset}";
+
+echo -e "${green}${bold}USAGE:${reset}"
+echo -e "${green}./ytcli.sh [music name]${reset}"
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Please read the usage"
+    exit 1
+fi
 
 query=$(printf '%s' "$*" | tr ' ' '+')
 
